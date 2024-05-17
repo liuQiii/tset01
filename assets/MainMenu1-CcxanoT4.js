f10f1081fcf49214ed1e13c57f886070a7233f8d
liuQiii <119015400+liuQiii@users.noreply.github.com>
2024-05-17 22:16:25 +0800

    test

---
+++ assets/MainMenu1-CcxanoT4.js
@@ -0,0 +1,1 @@
+import{d as s,o as e,c as a,F as t,g as n,a as o,_ as c}from"./index-DhoHASli.js";const r={class:"container"},_={class:"item"},i=s({__name:"MainMenu1",setup(d){return(p,l)=>(e(),a("div",r,[(e(),a(t,null,n(500,m=>o("div",_)),64))]))}}),f=c(i,[["__scopeId","data-v-a5e2539c"]]);export{f as default};
---
+++ assets/MainMenu2-BmvNbd0D.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,a as t}from"./index-DhoHASli.js";const a={},c={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",c,_)}const u=e(a,[["render",r]]);export{u as default};
